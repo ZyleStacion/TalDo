355486001bd5cc65073d28c86bd1d759c8847e14
ZyleStacion <s4064846@rmit.edu.vn>
2026-04-01 11:08:34 +0700

    feat/ store tasks from form data

--- app.ts
+++ app.ts
@@ -3,11 +3,11 @@ import express from "express";
 // Source - https://stackoverflow.com/a/64383997
 // Posted by adlopez15
 // Retrieved 2026-03-31, License - CC BY-SA 4.0
-
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 import path  from "path";
+import { body, validationResult } from "express-validator";
 
 const app = express();
 const PORT = 3000;
@@ -16,11 +16,23 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
     res.status(500);
+    res.render('index');
+})
+
+const tasks: { task: any; category: any; priority: any; }[] = [];
+
+app.post("/add-task", (req, res) =>{
+    // Store the task as a JSON object
+    const { task, category, priority } = req.body;
+    tasks.push({ task, category, priority });
+    console.log(tasks);
+    res.status(201);
     res.render('index');
 })
 
