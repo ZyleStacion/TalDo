c6140fdfe73b2f6cabefe6729e9a4f5c82ee0235
ZyleStacion <s4064846@rmit.edu.vn>
2026-04-02 14:20:30 +0700

    fix/ update task item backend logic to complete task and delete tasks

--- app.ts
+++ app.ts
@@ -28,6 +28,8 @@ app.get("/", (req, res) => {
 })
 
 app.post("/add-task", (req, res) =>{
+    // TODO: Use a unique ID instead of unique names
+    
     // Store the task as a JSON object
     const { task, category, priority } = req.body;
     tasks.push({ task, category, priority });
@@ -35,6 +37,26 @@ app.post("/add-task", (req, res) =>{
 
     // Redirect back to index (now with tasks)
     res.redirect('/');
+})
+
+app.post("/complete-task", (req, res) => {
+    console.log("Completed task!");
+
+    // Render alert
+
+    // Update task list
+})
+
+app.post("/delete-task", (req, res) => {
+    console.log("Delete task");
+    // Remove the task from the array
+    const { taskRemove, category, priority }  = req.body;
+    console.log("Task to remove: ", taskRemove);
+    const index = tasks.findIndex(task => task.task == taskRemove);
+    
+    if (index != -1) {
+        tasks.splice(index, 1);
+    }
 })
 
 app.listen(PORT, (error) => {
